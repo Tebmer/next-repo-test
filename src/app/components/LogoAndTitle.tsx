141fa4a7f764ac87cd2080c54162502b100a5d1c
accpatrick <106041998+accpatrick@users.noreply.github.com>
2025-02-22 02:31:59 +0800

    Update LogoAndTitle.tsx

--- src/app/components/LogoAndTitle.tsx
+++ src/app/components/LogoAndTitle.tsx
@@ -15,7 +15,7 @@ const LogoAndTitle = () => {
       </div>
 
       {/* Title */}
-      <h1 className="text-4xl font-extrabold mb-6 bg-gradient-to-r from-[#D2E0FB] via-[#8967B3] to-[#F05A7E] bg-clip-text text-transparent">
+      <h1 className="text-4xl font-extrabold mb-6 bg-gradient-to-r from-[#D2E0FB] via-[#8967B3] to-[#FF9874] bg-clip-text text-transparent">
         LiveSQLBench
       </h1>
     </div>
