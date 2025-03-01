0b9095e8ed472b69eba78a946d75ab2db8197cb0
accpatrick <106041998+accpatrick@users.noreply.github.com>
2025-03-01 21:02:18 +0800

    Update LatestNews.tsx

--- src/app/components/LatestNews.tsx
+++ src/app/components/LatestNews.tsx
@@ -10,19 +10,23 @@ const LatestNews = () => {
         <div className="h-0.5 flex-grow bg-gray-200"></div>
       </div>
       <div className="font-medium">
-        <div className="flex items-start gap-1">
-          <Image
-            src="/live_button.png"
-            alt="livesqlbench"
-            width={40}
-            height={40}
-            className="w-10 h-10 mt-0.5 mr-1 flex-shrink-0"
-          />
-          <div className="flex items-baseline">
-            <span className="text-gray-500 font-bold mr-2">[02/11/2025]:</span>
-            <span className="text-gray-700">
-              LiveSQLBench has been released! It contains two versions: LiveSQLBench-Base and LiveSQLBench-Large. Download it and test your text-to-SQL LLMs or agents in a containmation-free way!
-            </span>
+        <div className="flex">
+          <div className="flex-shrink-0 w-10 flex items-center self-start mt-1">
+            <Image
+              src="/live_button.png"
+              alt="livesqlbench"
+              width={40}
+              height={40}
+              className="w-10 h-10"
+            />
+          </div>
+          <div className="flex-grow ml-2">
+            <div className="flex flex-wrap items-baseline">
+              <span className="text-gray-500 font-bold mr-2">[02/11/2025]:</span>
+              <span className="text-gray-700">
+                LiveSQLBench has been released! It contains two versions: LiveSQLBench-Base and LiveSQLBench-Large. Download it and test your text-to-SQL LLMs or agents in a containmation-free way!
+              </span>
+            </div>
           </div>
         </div>
       </div>
