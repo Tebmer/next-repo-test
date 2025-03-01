177f1c9e1d47d2e932182a76b3d260fc47c542fd
accpatrick <106041998+accpatrick@users.noreply.github.com>
2025-03-01 20:51:36 +0800

    Update LatestNews.tsx

--- src/app/components/LatestNews.tsx
+++ src/app/components/LatestNews.tsx
@@ -10,13 +10,13 @@ const LatestNews = () => {
         <div className="h-0.5 flex-grow bg-gray-200"></div>
       </div>
       <div className="font-medium">
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <Image
             src="/live_button.png"
             alt="livesqlbench"
             width={40}
             height={40}
-            className="w-10 h-10"
+            className="w-10 h-10 flex-shrink-0"
           />
           <span className="text-gray-500 font-bold">[02/11/2025]:</span>
           <div>
