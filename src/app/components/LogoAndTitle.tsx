d5bb88f4cf19fc0d784bdda3f6af1528f3a3c577
accpatrick <106041998+accpatrick@users.noreply.github.com>
2025-03-01 19:55:00 +0800

    Update LogoAndTitle.tsx

--- src/app/components/LogoAndTitle.tsx
+++ src/app/components/LogoAndTitle.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 const LogoAndTitle = () => {
   return (
-    <div className="text-center mb-6">
+    <div className="text-center mb-4">
       {/* Logo */}
-      <div className="w-128 h-128 mx-auto mb-2 relative">
+      <div className="w-80 h-80 mx-auto relative mb-0">
         <Image
           src="/livesqlbench.png"
           alt="LiveSQLBench logo showing a cloud with SQL text inside"
@@ -14,8 +14,8 @@ const LogoAndTitle = () => {
         />
       </div>
 
-      {/* Title */}
-      <h1 className="text-5xl font-extrabold mb-4 bg-gradient-to-r from-[#F95454] to-[#0D92F4] bg-clip-text text-transparent">
+      {/* Title - positioned closer to the logo */}
+      <h1 className="text-5xl font-extrabold mt-0 bg-gradient-to-r from-[#F95454] to-[#0D92F4] bg-clip-text text-transparent">
         LiveSQLBench
       </h1>
     </div>
